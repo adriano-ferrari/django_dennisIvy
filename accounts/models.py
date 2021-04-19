bac762df46437046d1078746ae27764f93638284
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2021-04-19 17:56:22 -0300

    Database Models & Admin Panel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    email = models.EmailField(max_length=50, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=100, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    status = models.CharField(max_length=100, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True, choices=STATUS)
+
