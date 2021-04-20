5579be4278d21bd32e855a26f04a137ca4b39c33
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2021-04-19 21:29:47 -0300

    Rendering Data to Templates | Template Tags

--- accounts/models.py
+++ accounts/models.py
@@ -44,4 +44,3 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=100, null=True, choices=STATUS)
     created = models.DateTimeField(auto_now_add=True, null=True)
-
